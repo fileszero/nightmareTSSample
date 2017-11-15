40d85423e02b4017d52242fd9b1d8d880e7577ac
FILES=0 <files.eq.zero@gmail.com>
2017-11-15 14:06:37 +0900

    add forevertest

---
+++ src/forevertest.ts
@@ -0,0 +1,30 @@
+import * as log4js from "log4js";
+import * as config from "config";
+
+// please install forever
+// > npm install forever -g
+// execute
+// > npm run forevertest
+
+log4js.configure(config.get("log4js.configure"));
+const logger = log4js.getLogger();
+logger.level = "debug"; // don't show trace message
+
+/**
+ * wait ms
+ */
+function delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function main() {
+    let counter = 0;
+    while (true) {
+        logger.info("counter = " + counter);
+        counter++;
+        await delay(5000);
+    }
+}
+
+main().then(() => {
+    logger.info("Done");
+});
